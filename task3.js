e5dde1e2f525e855acb37b8d90396f005e52d9db
falahadeel-ctrl <falahadeel@gmail.com>
2025-12-06 22:23:06 -0500

    second

--- task3.js
+++ task3.js
@@ -1,17 +1,36 @@
-// let z='#';
-// for(let x=0; x<7; x++){
-//     z=z+'#';
-// console.log(z);
-// }
+let string=`ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26`;
 
-let string = `ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz 
-Master,58\n98,Bill,Doctor’s Assistant,26`;
-let z = "  ";
-for(let i= 0; i < string.length; i++){
-    if(string[i] == "\n"){
-    console.log(z);
-    }
-    else if(string[i] == ","){
-    console.log(z);
-    }
+let cell1="";
+let cell2="";
+let cell3="";
+let cell4="";
+let comma = 0;
+
+for(let i =0;i<string.length; i++){
+    let text= string[i];
+    if(text==","){
+  comma++;
+    }else if(text=="\n"){
+        console.log(cell1,cell2,cell3,cell4)
+cell1="";
+cell2="";
+cell3="";
+cell4="";
+comma=0;
+    }else
+        if(comma == 0){
+    cell1=cell1+text;
+        }else if(comma == 1){
+    cell2=cell2+text;
+        }else if(comma == 2){
+    cell3=cell3+text;
+        }else {
+    cell4=cell4+text;
+        }
+        if(i+1==string.length){
+    console.log(cell1,cell2,cell3,cell4);
 }
+}
+
+
+
